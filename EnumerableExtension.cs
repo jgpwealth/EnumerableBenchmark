4fe7d1cbad2224ccf611c6c9d9f9dc30e99f90b2
FeNS <fnogueirasouza@gmail.com>
2023-01-09 15:58:20 -0300

    Add files via upload

---
+++ EnumerableExtension.cs
@@ -0,0 +1,31 @@
+﻿namespace EnumerableBenchmarks
+{
+    public static class EnumerableExtension
+    {
+        /// <summary>
+        /// Generic Divide Large Group into Smaller Ones with Size = chunkSize
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="bigGroup"></param>
+        /// <param name="chunkSize"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Divide<T>(this IEnumerable<T> bigGroup, int chunkSize)
+        {
+            if (bigGroup == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (chunkSize < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            while (bigGroup.Any())
+            {
+                yield return bigGroup.Take(chunkSize);
+                bigGroup = bigGroup.Skip(chunkSize);
+            }
+        }
+    }
+}
---
+++ EnumerableExtensionsBenchmarks.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using EnumerableBenchmarks;
+
+namespace EnumerableExtensionsBenchmarks
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class Benchmarks
+    {
+        private IEnumerable<int> bigGroup = Enumerable.Range(0, 99999);
+        private int chunkSize = 77;
+
+        [Benchmark]
+        public void Divide()
+        {
+            bigGroup.Divide(chunkSize).ToList();
+        }
+
+        [Benchmark]
+        public void Chunk()
+        {
+            bigGroup.Chunk(chunkSize).ToList();
+        }
+    }
+}
